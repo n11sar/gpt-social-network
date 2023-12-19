41c0873dc6ea61272031c9a793ba5ae1c172f2ee
georgy <ganus1111@gmail.com>
2023-12-19 15:29:57 +0200

    feat: init comit

---
+++ src/main/java/org/example/gptsocialnetwork/GptSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package org.example.gptsocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptSocialNetworkApplication.class, args);
+    }
+
+}
