857c3b73d4c80084f87790d88cc7ba4f8682b57e
georgy <ganus1111@gmail.com>
2023-12-20 09:45:21 +0200

    feat: some fixes 2

--- src/main/java/org/example/gptsocialnetwork/repositories/PostLikesRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.gptsocialnetwork.repositories;
-
-import org.example.gptsocialnetwork.entities.PostLikes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PostLikesRepository extends JpaRepository<PostLikes,Long> {
-
-}
--- src/main/java/org/example/gptsocialnetwork/repositories/UserFollowersRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.gptsocialnetwork.repositories;
-
-import org.example.gptsocialnetwork.entities.UserFollowers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserFollowersRepository extends JpaRepository<UserFollowers, Long> {
-
-}
